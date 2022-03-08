cb86f8881a7c0976ec5afb032c1ff413c4564008
Lucas Gabriel Ferreira <lgf.lucas.ferreira.gabriel@gmail.com>
2022-03-08 11:38:18 -0300

    Arrumando meus Projetos
    Calculador de Média e Conversor de Moedas

---
+++ Calculador de Média/vars.js
@@ -0,0 +1,23 @@
+var nome = "Lucas"
+
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaTerceiroBimestre = 4
+var notaQuartoBimestre = 2
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem Vindo " + nome)
+console.log(notaFixada)
+
+// Revisão:
+//Variáveis, String, console.log, .toFixed, operações matemáticas, Concatenação, Console
+
+
+//Desafios:
+//Falar se o aluno foi ou não aprovado.
+//Mudar o CSS
+//Colocar na tela a nota
+//Fazer um conversor de temperatura/moeda/milhas, Tanto faz
